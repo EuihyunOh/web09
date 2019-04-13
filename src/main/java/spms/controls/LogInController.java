1432ce6f5aab55b1de627d42fe85d6c1edbc205f
Aqours <poweroeh@seoultech.ac.kr>
2019-04-13 19:06:49 +0900

    Use spring IoC container
    But error in beans.xml

--- src/main/java/spms/controls/LogInController.java
+++ src/main/java/spms/controls/LogInController.java
@@ -4,7 +4,8 @@ import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
-import spms.annotation.Component;
+import org.springframework.stereotype.Component;
+
 import spms.bind.DataBinding;
 import spms.dao.MySqlMemberDao;
 import spms.vo.Member;
